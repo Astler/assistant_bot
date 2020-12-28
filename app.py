2b18a92b9aefe71f4755d8f8b6a50aa33964a672
Astler <vladyclaw@gmail.com>
2020-12-28 10:24:11 +0200

    IDK 11

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from utils.set_bot_commands import set_default_commands
-
+from loader import bot, dp
+import logging
+from aiogram import types
 from aiogram.utils.executor import start_webhook
 from data.config import (BOT_TOKEN, HEROKU_APP_NAME,
                           WEBHOOK_URL, WEBHOOK_PATH,
@@ -15,18 +17,21 @@ async def on_startup(dp):
     await on_startup_notify(dp)
     await set_default_commands(dp)
 
+    logging.warning(
+        'Starting connection. ')
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
 
 if __name__ == '__main__':
-    from aiogram import executor
+    from aiogram import executor, types
     from handlers import dp
 
-    start_webhook(
+    executor.start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
         on_startup=on_startup,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
 
-    executor.start_polling(dp, on_startup=on_startup)
+    #executor.start_polling(dp, on_startup=on_startup)
