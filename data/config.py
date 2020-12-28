eee4c0c9d066ad65c41e3c0e42176efc065459d8
Astler <vladyclaw@gmail.com>
2020-12-28 20:23:41 +0200

    IDK 22

--- data/config.py
+++ data/config.py
@@ -13,7 +13,7 @@ if not BOT_TOKEN:
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_HOST = f'https://ashebottelegram.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
