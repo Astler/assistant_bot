7dcc271138c25e9c411a4e6cec91fa330df252b2
Astler <vladyclaw@gmail.com>
2020-12-28 19:04:03 +0200

    IDK 15

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from aiogram.utils.executor import start_webhook
 from utils.set_bot_commands import set_default_commands
 from loader import bot, dp
 import logging
-from aiogram import types
+from aiogram import types, md
 from aiogram.dispatcher.webhook import get_new_configured_app
 
 from data.config import (BOT_TOKEN, HEROKU_APP_NAME,
@@ -37,19 +37,32 @@ async def on_shutdown(dp):
     logging.warning("Bot down")
 
 
+@dp.message_handler(commands='start')
+async def welcome(message: types.Message):
+    await bot.send_message(
+        message.chat.id,
+        f'Приветствую! Это демонтрационный бот\n'
+        f'Подробная информация на '
+        f'{md.hlink("github", "https://github.com/deploy-your-bot-everywhere/heroku")}',
+        parse_mode=types.ParseMode.HTML,
+        disable_web_page_preview=True)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.chat.id, message.text)
+
+
 if __name__ == '__main__':
     from aiogram import executor, types
     from handlers import dp
 
-    # if "HEROKU" in list(os.environ.keys()):
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
+                  on_startup=on_startup, on_shutdown=on_shutdown,
+                  host=WEBAPP_HOST, port=WEBAPP_PORT)
+
+
+# if "HEROKU" in list(os.environ.keys()):
 # else:
 #     executor.start_polling(dp)
+
