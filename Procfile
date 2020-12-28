6264040192c4469faf1925338b8587a411c10c62
Astler <vladyclaw@gmail.com>
2020-12-28 19:26:42 +0200

    IDK 16

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py runserver
+bot: python app.py
--- app.py
+++ app.py
@@ -1,68 +1,30 @@
-import os
-
-from aiogram.utils.executor import start_webhook
-
 from utils.set_bot_commands import set_default_commands
 from loader import bot, dp
 import logging
-from aiogram import types, md
-from aiogram.dispatcher.webhook import get_new_configured_app
-
+from aiogram import types
+from aiogram.utils.executor import start_webhook
 from data.config import (BOT_TOKEN, HEROKU_APP_NAME,
-                         WEBHOOK_URL, WEBHOOK_PATH,
-                         WEBAPP_HOST, WEBAPP_PORT)
-
+                          WEBHOOK_URL, WEBHOOK_PATH,
+                          WEBAPP_HOST, WEBAPP_PORT)
 
 async def on_startup(dp):
-    await bot.delete_webhook()
-    await bot.set_webhook(WEBHOOK_URL)
-
     import filters
     import middlewares
     filters.setup(dp)
     middlewares.setup(dp)
-    logging.info(dp)
+
     from utils.notify_admins import on_startup_notify
     await on_startup_notify(dp)
     await set_default_commands(dp)
 
-
-
-# Run before shutdown
-async def on_shutdown(dp):
-    logging.warning("Shutting down..")
-    await bot.delete_webhook()
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-    logging.warning("Bot down")
-
-
-@dp.message_handler(commands='start')
-async def welcome(message: types.Message):
-    await bot.send_message(
-        message.chat.id,
-        f'Приветствую! Это демонтрационный бот\n'
-        f'Подробная информация на '
-        f'{md.hlink("github", "https://github.com/deploy-your-bot-everywhere/heroku")}',
-        parse_mode=types.ParseMode.HTML,
-        disable_web_page_preview=True)
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(message.chat.id, message.text)
+    logging.warning(
+        'Starting connection. ')
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 
 if __name__ == '__main__':
     from aiogram import executor, types
     from handlers import dp
 
-    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
-                  on_startup=on_startup, on_shutdown=on_shutdown,
-                  host=WEBAPP_HOST, port=WEBAPP_PORT)
-
-
-# if "HEROKU" in list(os.environ.keys()):
-# else:
-#     executor.start_polling(dp)
+    executor.start_polling(dp, on_startup=on_startup)
 
