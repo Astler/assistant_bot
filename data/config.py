6c177c6567c8e29529473b6f55c40dc0d509a66f
Astler <vladyclaw@gmail.com>
2020-12-28 10:09:12 +0200

    IDK 10

--- data/config.py
+++ data/config.py
@@ -20,7 +20,7 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = int(os.getenv('PORT'))
+WEBAPP_PORT = int(str(os.getenv('PORT')))
 
 admins = [
     376225089
