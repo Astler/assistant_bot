f5c9c3b8f7ce85208de70c132aa09daa46305cda
Astler <vladyclaw@gmail.com>
2020-12-28 22:15:50 +0200

    hof

--- handlers/channels/hof.py
+++ handlers/channels/hof.py
@@ -5,7 +5,9 @@ from filters import IsPrivate, IsGroup
 
 from data.config import admins
 
-import urllib.request, json
+import urllib.request
+
+import json
 
 
 @dp.message_handler(IsGroup(), commands="hof")
@@ -14,7 +16,15 @@ async def bot_get_hof(message: types.Message):
 
     with urllib.request.urlopen("https://geekstand.top/hof.json") as url:
         data = json.loads(url.read().decode())
-        print(data)
+
+        json_array = json.load(data)
+        store_list = []
+
+        for item in json_array:
+            store_details = {'mName': item['mName'], 'mDescription': item['mDescription']}
+            store_list.append(store_details)
+
+        await message.answer(str(store_list))
 
 
 @dp.message_handler(commands="secret", user_id=admins)
