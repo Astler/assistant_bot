085353b492da3e43bcc41a90f6a33e2dc9670191
Vladislav Terentyev <v.terentyev@themindstudios.com>
2022-01-02 18:52:02 +0200

    0.3.3.1

--- filters/is_rep_msg.py
+++ filters/is_rep_msg.py
@@ -1,8 +1,21 @@
 from aiogram import types
 from aiogram.dispatcher.filters import BoundFilter
 
+positive_rep = [
+    "+rep",
+    "+ rep",
+    "+реп",
+    "+ реп",
+]
+
+negative_rep = [
+    "-rep",
+    "- rep",
+    "-реп",
+    "- реп",
+]
+
 
 class IsRepMsg(BoundFilter):
     async def check(self, message: types.Message):
-        return message.text.__contains__("+rep") or message.text.__contains__("-rep") or message.text.__contains__(
-            "+реп") or message.text.__contains__("-реп")
+        return any(word in message.text for word in positive_rep) or any(word in message.text for word in negative_rep)
