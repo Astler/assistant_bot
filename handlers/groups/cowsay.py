140c11628b4e14a953f68ac7650370f2ab26ee57
Vladislav Terentyev <v.terentyev@themindstudios.com>
2022-03-16 23:19:23 +0200

    0.3.8.5

--- handlers/groups/cowsay.py
+++ handlers/groups/cowsay.py
@@ -11,9 +11,24 @@ from loader import dp, bot
 
 @dp.message_handler(commands="cowsay")
 async def cowsay_say(message: types.Message):
-    text = message.get_args()
+    args = message.get_args().split(" ")
+
+    if len(args) == 0:
+        return
+
+    if len(args) == 1:
+        text = str(args)
+        character = "cow"
+    else:
+        text = str(args[0])
+        character = str(args[1])
 
     if len(text) != 0:
-        result = cowsay.get_output_string('cow', text)
+        result = cowsay.get_output_string(character, text)
         print(result)
         await bot.send_message(message.chat.id, "```" + result + "```", parse_mode="Markdown")
+
+
+@dp.message_handler(commands="cowsay_help")
+async def cowsay_say(message: types.Message):
+    await bot.send_message(message.chat.id, cowsay.char_names)
