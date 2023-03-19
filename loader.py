0740f4b0608cd38351a0c3594e4811578c812328
Vladislav Terentyev <v.terentyev@themindstudios.com>
2023-03-19 21:44:06 +0200

    0.4.5.3

--- loader.py
+++ loader.py
@@ -4,7 +4,7 @@ from github import Github
 from pyrogram import Client
 
 from data import config
-from data.config import GITHUB_TOKEN, GITHUB_REPO, BOT_TOKEN, API_ID, API_HASH
+from data.config import GITHUB_TOKEN, GITHUB_REPO, BOT_TOKEN, API_ID, API_HASH, INSTANCE_UNIQUE_NAME
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
@@ -14,8 +14,8 @@ github = Github(GITHUB_TOKEN)
 repository = github.get_user().get_repo(GITHUB_REPO)
 
 app = Client(
-        "cat_pyro",
-        bot_token=BOT_TOKEN,
-        api_id=API_ID,
-        api_hash=API_HASH
-    )
+    INSTANCE_UNIQUE_NAME,
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
